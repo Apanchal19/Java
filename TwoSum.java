9e099228f082783df6577b30e3a71b55b8d63b7e
“apanchal19” <akshay.panchalhp@gmail.com>
2023-01-16 23:48:24 -0500

    1st edit

---
+++ TwoSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.math.*;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        // int i;       
+        // int n = 4;
+
+        // for(i=0; i <= n; i++);
+        //     System.out.println("*\n");
+    
+    
+    Scanner sc = new Scanner(System.in);
+    int radius = sc.nextInt();
+    // int b = sc.nextInt();
+    int area =  (int) ((Math.PI) * (radius*radius));
+    System.out.println(area);
+    
+    }
+}
